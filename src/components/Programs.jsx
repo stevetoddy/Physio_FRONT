6d521d75df2cd6d3cacd373c010b5255ef809e61
kcampbell3190 <kcampbell3190@gmail.com>
2023-02-03 17:41:28 +1100

    style changes

--- src/components/Programs.jsx
+++ src/components/Programs.jsx
@@ -20,7 +20,8 @@ const Programs = ({ userPrograms }) => {
     return (
         <>
             <Navbar />
-            
+       
+
             {userPrograms.length > 0 && 
             <ul className="list-group">
                 {userPrograms.map((program, index) => (
