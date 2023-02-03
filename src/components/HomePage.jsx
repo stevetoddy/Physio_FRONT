d087f26ef1323c2b39d28b8eca7fd1d3fa5bde97
kcampbell3190 <kcampbell3190@gmail.com>
2023-02-03 11:14:31 +1100

    added landing page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Avatar from './Images/avatar.png'
 
 const Home = () => {
     return (
         <>
+
+<img className="avatar" src={Avatar}></img>
             <h1>HOME PAGE</h1>
             <ul>
                 <li><Link to={`/`}>Home</Link></li>
---
+++ src/components/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Landing.css'
+
+
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div>Landing</div>
+  )
+}
+
+export default Landing
+
