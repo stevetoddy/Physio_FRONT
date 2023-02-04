24780793289e57e51eaa2a235b5b1ee18c062d46
kcampbell3190 <kcampbell3190@gmail.com>
2023-02-04 11:04:34 +1100

    fixed height issues'

--- src/components/Programs.jsx
+++ src/components/Programs.jsx
@@ -1,35 +1,25 @@
 import { React } from 'react'
 import { Link } from 'react-router-dom'
 import Navbar from './Navbar'
+import './Programs.css'
 
-const styles = {
-    background: 'lightgrey',
-    padding: '0.5rem',
-    // margin: '0.5rem 2rem',
-    borderRadius: '0.5rem',
-    color: "black",
-}
 
-const linkStyles = {
-    textDecoration: 'none',
-    color: 'darkBlue'
-
-}
 
 const Programs = ({ userPrograms }) => {
     return (
         <>
             <Navbar />
        
-
+        <div className="parent-div">
             {userPrograms.length > 0 && 
             <ul className="list-group">
                 {userPrograms.map((program, index) => (
                     <div key={index}>
-                        <h4 style={styles} className="list-group-item"><Link style={linkStyles} to={`/programs/${program._id}`} >{program.name}</Link></h4>
+                        <h4  className="list-group-item"><Link  to={`/programs/${program._id}`} >{program.name}</Link>Hello</h4>
                     </div>
                 ))}
             </ul>}
+       </div>
         </>
     )
 }
